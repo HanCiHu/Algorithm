3da164fbaabdfa76290357fdbf68b5a50bd71b0e
cyu <kinm1596@daum.net>
2020-09-28 21:32:56 +0900

    subject

--- subject/4주차/1.py
+++
@@ -1,8 +0,0 @@
-def func(s1, s2):
-	if ''.join(sorted(s1.lower())) == ''.join(sorted(s2.lower())):
-		return True
-	else:
-		return False
-
-s1, s2 = map(str, input().split(' '))
-print(func(s1,s2))
