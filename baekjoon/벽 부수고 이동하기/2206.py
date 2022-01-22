259c01bfd91f23408fb44b43bbd77fb431cb2135
cyu <kinm1596@daum.net>
2022-01-22 21:06:48 +0900

    ✨ [2022.01.22]2206.py

--- baekjoon/벽 부수고 이동하기/2206.py
+++ baekjoon/벽 부수고 이동하기/2206.py
@@ -4,36 +4,30 @@ import copy
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-def countDis(maps):
-  visit = [[0 for _ in range(M)] for _ in range(N)]
-  q = deque([(0,0)])
+def bfs():
+  visit = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+  q = deque([(0,0,0)])
 
   while q:
-    x, y = q.popleft()
+    x, y, isCrashed = q.popleft()
+
+    if x == M - 1 and y == N - 1:
+      return visit[y][x][isCrashed] + 1
 
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
       if nx >= 0 and nx < M and ny >= 0 and ny < N:
-        if maps[ny][nx] == '0' and visit[ny][nx] == 0:
-          q.append((nx,ny))
-          visit[ny][nx] = visit[y][x] + 1
-  return visit[N - 1][M - 1] + 1
-
-def breakWall():
-  ans = -1
-  for n in range(N):
-    for m in range(M):
-      if maps[n][m] == '1':
-        newMaps = copy.deepcopy(maps)
-        newMaps[n][m] = '0'
-        dis = countDis(newMaps)
-        if dis > 1 and dis > ans:
-          ans = dis
-  return ans
+        if maps[ny][nx] == '0' and visit[ny][nx][isCrashed] == 0:
+          q.append((nx,ny, isCrashed))
+          visit[ny][nx][isCrashed] = visit[y][x][isCrashed] + 1
+        if maps[ny][nx] == '1' and isCrashed == 0:
+          q.append((nx, ny, isCrashed + 1))
+          visit[ny][nx][isCrashed + 1] = visit[y][x][isCrashed] + 1
+  return -1
 
 
 N, M = map(int , input().split(' '))
 maps = [list(input()) for _ in range(N)]
 
-print(breakWall())
+print(bfs())
