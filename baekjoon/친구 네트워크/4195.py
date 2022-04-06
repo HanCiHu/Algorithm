6cb8dccf76296a3417978d37cbc48b599142555d
HanCiHu <kinm1596@daum.net>
2022-04-06 20:23:54 +0900

    ⚡️ [2022.04.06] 4195.py
    백준 골드2 친구 네트워크
    union & find 응용문제

--- baekjoon/친구 네트워크/4195.py
+++ baekjoon/친구 네트워크/4195.py
@@ -2,32 +2,32 @@ from collections import defaultdict
 
 T = int(input())
 
-def find(name):
+def find(parents, name):
   if name != parents[name]:
-    parents[name] = find(parents[name])
+    parents[name] = find(parents, parents[name])
   return parents[name]
 
-def union(name1, name2):
-  name1 = find(name1)
-  name2 = find(name2)
+def union(parents, cnt, name1, name2):
+  name1 = find(parents, name1)
+  name2 = find(parents, name2)
 
   if name1 != name2:
     parents[name2] = name1
-    rank[name1] += rank[name2]
-  print(rank[name1])
+    cnt[name1] = cnt[name2] + cnt[name1]
+  print(cnt[name1])
 
 for _ in range(T):
+  cnt = defaultdict(lambda : 1)
+  parents = {}
+
   N = int(input())
 
-  parents = {}
-  rank = defaultdict(lambda: 1)
-
-  for _ in range(N):
+  for i in range(N):
     name1, name2 = map(str, input().split(' '))
-    if parents.get(name1) == None:
+    if not name1 in parents:
       parents[name1] = name1
-    if parents.get(name2) == None:
+    if not name2 in parents:
       parents[name2] = name2
-    union(name1, name2)
 
-
+    union(parents, cnt, name1, name2)
+    
