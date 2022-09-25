e3dd1af4a15fc66170d10c0ac684e6a73f2bae68
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-09-26 03:57:13 +0900

    ⚡️ [2022.09.26] 256364.py
    전구 상태 뒤집기
    숏코딩 1등 달성

--- baekjoon/전구 상태 뒤집기/25634.py
+++ baekjoon/전구 상태 뒤집기/25634.py
@@ -1,17 +1,10 @@
 N = int(input())
-b = list(map(int, input().split(' ')))
-lights = list(map(int, input().split(' ')))
-
-points = 0
-
+b = list(map(int, input().split()))
+c = list(map(int, input().split()))
+a = 0
+for i in range(N):a+=b[i] if c[i] else 0
+dp=[-9e999 for _ in range(N)]
 for i in range(N):
-  if lights[i] == 1:
-    points += b[i]
-
-dp = [float('-inf') for _ in range(N + 1)]
-
-for i in range(N):
-  k = b[i] if lights[i] == 0 else -b[i]
-  dp[i] = max(dp[i - 1] + k, k)
-
-print(points + max(dp))
+  k=b[i] if c[i]==0 else -b[i]
+  dp[i]=max(dp[i-1]+k,k)
+print(a+max(dp))
