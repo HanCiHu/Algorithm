0a3e9d9ab098d06623f625fc644af99f5692f086
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-12-05 17:41:12 +0900

    ⚡️ [2022.12.05] RuleOfBigNum.py
    M의 크기가 10억 이상인 경우 최적화

--- thisIsCodingTest/큰 수의 법칙/RuleOfBigNum.py
+++ thisIsCodingTest/큰 수의 법칙/RuleOfBigNum.py
@@ -1,16 +1,24 @@
 a,b,c = map(int, input().split(' '))
 numArr = list(map(int, input().split(' ')))
 
-ans = 0
-flag = 0
-numArr.sort()
+def ans1():
+  ans = 0
+  flag = 0
+  numArr.sort()
 
-for i in range(b):
-  if flag < c:
-    ans += numArr[-1]
-    flag += 1
-  else:
-    ans += numArr[-2]
-    flag = 0
+  for i in range(b):
+    if flag < c:
+      ans += numArr[-1]
+      flag += 1
+    else:
+      ans += numArr[-2]
+      flag = 0
+  return ans
 
-print(ans)
+def ans2():
+  numArr.sort()
+  ans = numArr[-2] * (b // c) + numArr[-1] * (b - (b // c))
+  return ans
+
+print(ans1())
+print(ans2())
