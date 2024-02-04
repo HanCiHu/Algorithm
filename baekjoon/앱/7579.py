3c36836e255dac721a7035979d76a5234197ff08
HanCiHu <chanhui.yu.cnu@gmail.com>
2024-02-04 21:45:29 +0900

    [2024.02.04] 7579.py
    백준 골드 3
    앱
    배낭 DP 문제

--- baekjoon/앱/7579.py
+++ baekjoon/앱/7579.py
@@ -1,25 +1,16 @@
+N, M = map(int, input().split())
+mem = [0] + list(map(int, input().split()))
+cost = [0] + list(map(int, input().split()))
 
+dp = [[0 for _ in range(sum(cost) + 1)] for _ in range(N + 1)]
 
-N, M = map(int, input().split(' '))
-
-mem = [0] + list(map(int, input().split(' ')))
-cost = [0] + list(map(int, input().split(' ')))
-
-s = sum(cost) + 1
-dp = [[0 for _ in range(s)] for _ in range(N + 1)]
-
+ans = sum(cost)
 
 for i in range(1, N + 1):
-  for j in range(s):
-    if cost[i] <= j:
-      dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - cost[i]] + mem[i])
-    else:
-      dp[i][j] = dp[i - 1][j]
+  for j in range(0, sum(cost) + 1):
+    if j < cost[i]: dp[i][j] = dp[i - 1][j]
+    else: dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - cost[i]] + mem[i])
+    if dp[i][j] >= M: ans = min(ans, j)
 
-ans = float('inf')
-
-for i in range(1, len(dp)):
-  for j in range(1, len(dp[i])):
-    if dp[i][j] >= M:
-      ans = min(ans, j)
-print(ans)
+if M != 0: print(ans)
+else: print(0)
